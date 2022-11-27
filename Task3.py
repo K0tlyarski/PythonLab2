6d8fe467d5190cff4a27e89315a58b8658fd341d
K0tlyarski <djgame2.9.6@gmail.com>
2022-11-27 17:49:10 +0400

    Creating a function that returns the file name

--- Task3.py
+++ Task3.py
@@ -4,3 +4,7 @@ import os
 import shutil
 from Task2 import create_dir
 import csv
+
+def get_element(class_name:str)->generator:
+    for file_name in os.listdir(os.path.join("dataset", class_name)):
+        yield file_name
